c91f685690ba539aa86229b896c991da0f711dd8
Mohammed Usaith Abbas.S <pcasd21cs029@gmail.com>
2025-04-28 15:42:19 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(true)
   const [bubbles, setBubbles] = useState([])
   const [showForYouBtn, setShowForYouBtn] = useState(false)
-  const birthdayDate = new Date("April 28, 2025") // Change this date accordingly
+  const birthdayDate = new Date("May 2, 2025") // Change this date accordingly
   const audioRef = useRef(null)
 
   // For testing
